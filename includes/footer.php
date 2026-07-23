f9eae01740c12c250258c1f11f30c70adb4e5b1c
Lưu Nguyên Hùng <hung.2474802010127@vanlanguni.vn>
2026-07-23 17:15:20 +0700

    Include Bootstrap 5 JavaScript in footer.php
    Added Bootstrap 5 JavaScript bundle to footer.

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,4 @@
-
+<!-- Bootstrap 5 JavaScript Bundle (Bắt buộc để chạy Modal, Dropdown, v.v.) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
